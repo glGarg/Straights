e81202e591ab49cb7be3625a37e3085f643319e7
Spandan Garg <spandan.garg1706@gmail.com>
2017-06-14 03:31:18 -0400

    Title: Remove unwanted files Summary: ^

--- Straights/src/shuffle.cc
+++
@@ -1,8 +0,0 @@
-/* 
- * To ensure that the your cards are ordered the same way as
- * the given program, use this shuffling algorithm.
- *
- * CARD_COUNT is the constant 52
- * cards_ is an array of pointers to cards
-*/
-
